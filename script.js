4d239d6a1a4bfee1f9f337cda76a26d6d72c146e
RUPAM METE <166600502+Rupam-Mete@users.noreply.github.com>
2025-02-23 21:46:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
 
-    if (email === 'rrr' && password === 'hii') {
+    if (email === 'rush' && password === 'pagli') {
         window.location.href = 'success.html';
     } else {
         window.location.href = 'error.html';
